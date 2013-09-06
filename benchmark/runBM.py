b10d75dab47f3361689f923396aa07fabcab449c
sherry <siyuan.sherryshare.wang@gmail.com>
2013-09-06 17:04:50 +0800

    runBM.py moves to benchmark dir with sysargv.

--- benchmark/runBM.py
+++ benchmark/runBM.py
@@ -1,5 +1,6 @@
 import subprocess
 import os
+import sys
 
 def bubblesort(numbers):
     for j in range(len(numbers)-1,-1,-1):
@@ -15,37 +16,45 @@ if __name__ == '__main__':
 	path = execute_cmd('pwd').strip('\n')
 	if not os.path.exists(r'%s/build' %path):
 		execute_cmd('cd %s; mkdir build;' %path)
-	cmd = 'cd %s/build; rm -rf *;cmake ../; make;' % path
+	path = '%s/build' %path
+	cmd = 'cd %s; rm -rf *;cmake ../; make;' % path
 	execute_cmd(cmd)
-	matrix_n = 10
-	rand_max = 5
-	#print 'file:random'
-	#execute_cmd('cd %s/build/benchmark; ./random %s %s' % (path,matrix_n,rand_max))
 
-	file_all = execute_cmd('ls %s/build/benchmark' % path)
+	file_all = execute_cmd('ls %s' % path)
 	file_list = [line for line in file_all.split('\n')]
+	if len(sys.argv) > 1:
+		for item in file_list:
+			if item not in sys.argv:
+				#Shouldn't remove the item or the pointer may change, thus not deleting all the unmatched.
+				file_list[file_list.index(item)]=""
+	
+	#Get the random matrix
+	if 'lu' in file_list:
+		matrix_n = 10
+		rand_max = 5
+		print 'file:random'
+		execute_cmd('cd %s; ./random %s %s' % (path,matrix_n,rand_max))
+	#End of get the random matrix
 
 	for item in file_list:
-		ifile = r'%s/build/benchmark/%s' % (path, item)
+		ifile = r'%s/%s' % (path, item)
 		if os.path.isfile('%s' %ifile) and os.access('%s' % ifile, os.X_OK):
 			if not cmp(item,'random'):
 				continue
 			elif not cmp(item,'lu'):
-				continue
-				#para_n = matrix_n / 2
+				para_n = matrix_n / 2
 			elif not cmp(item,'quicksort'):
 				para_n = 4
 			elif not cmp(item,'canny'):
-				#continue
 				para_n = 1
 			print 'file:%s' % item
 			print 'Parallel time:'
-			time_path = '%s/build/benchmark/para_time.txt' % path
+			time_path = '%s/para_time.txt' % path
 			if os.path.exists(r'%s' %time_path):
 				execute_cmd('rm %s' % time_path)
 			cycle = 3;
 			for i in range(cycle):
-				print execute_cmd('cd %s/build/benchmark; ./%s %s' % (path, item, para_n)).strip('\n')
+				print execute_cmd('cd %s; ./%s %s' % (path, item, para_n)).strip('\n')
 			time_file = open(time_path)
 			ptime = 0
 			etime = 1
@@ -63,8 +72,8 @@ if __name__ == '__main__':
 			print 'Geometric mean time: ' + str(etime) + 'us'
 			time_file.close()
 			execute_cmd('rm %s' % time_path)
-			execute_cmd('cd %s/build/benchmark; ./%s' % (path, item))			
-			time_path2 = '%s/build/benchmark/time.txt' % path
+			execute_cmd('cd %s; ./%s' % (path, item))			
+			time_path2 = '%s/time.txt' % path
 			time_file = open(time_path2)
 			print 'Serial time:' + time_file.read().strip('\n') + 'us'
 			time_file.close()
