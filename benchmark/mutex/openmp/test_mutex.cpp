d952dbbd06d70b80b68983e81eca2f900daff5f6
sherry <siyuan.sherryshare.wang@gmail.com>
2013-12-12 17:55:09 +0800

    mutex on openmp added.

--- benchmark/mutex/openmp/test_mutex.cpp
+++ benchmark/mutex/openmp/test_mutex.cpp
@@ -1,6 +1,4 @@
-#include <tbb/mutex.h>
-#include <tbb/task_scheduler_init.h>
-#include <tbb/task_group.h>
+#include <omp.h>
 #include <cmath>
 #include <chrono>
 #include <iostream>
@@ -11,10 +9,10 @@
 #include <mutex>
 
 using namespace std;
-using namespace tbb;
 
-typedef tbb::mutex TMutex;
-typedef std::shared_ptr<TMutex> TMutex_ptr;
+typedef omp_lock_t TMutex;
+//typedef std::shared_ptr<TMutex> TMutex_ptr;
+typedef TMutex * TMutex_ptr;
 typedef std::shared_ptr<int64_t> Res_ptr;
 std::vector<TMutex_ptr> ms;
 std::vector<Res_ptr> rs;
@@ -23,8 +21,10 @@ typedef std::mutex SMutex;
 typedef std::shared_ptr<SMutex> SMutex_ptr;
 std::vector<SMutex_ptr> std_ms;
 
+
 //const int LOOP_TIMES = 1000;
 const int LOOP_TIMES = 30;
+
 
 int fib(int n)
 {
@@ -44,11 +44,10 @@ void task_fun(int j) {
     {
 //         random_fib();
         fib(10-j);
-        TMutex::scoped_lock lock;//create a lock
-        lock.acquire(*(ms[j]));
+        omp_set_lock(ms[j]);
 //         *(rs[j]) += random_fib();
         *(rs[j]) += fib(15+j);
-        lock.release();
+        omp_unset_lock(ms[j]);
     }
 }
 
@@ -99,10 +98,10 @@ bool write_time_file(int elapsed_seconds, bool bIsPara) {
 
 int main(int argc, char *argv[])
 {
-    task_scheduler_init init;
+    omp_set_num_threads(8);
     bool bIsPara = false,bIsStd = false;//false;
     int elapsed_seconds;
-    int concurrency = task_scheduler_init::default_num_threads();
+    int concurrency = omp_get_max_threads();
     if(argc > 1) {
         stringstream ss_argv;
         int n;// n > 0 means parallel, otherwise serial.
@@ -112,47 +111,48 @@ int main(int argc, char *argv[])
     }
     if(argc > 2)
         bIsStd = true;
-
     if(bIsPara) {
+
         for(int i = 0; i< concurrency; i++)
         {
             if(bIsStd)
                 std_ms.push_back(std::make_shared<SMutex>());
-            else
-                ms.push_back(std::make_shared<TMutex>());
+            else {
+                TMutex lock;
+//                 ms.push_back(std::make_shared<TMutex>());
+                ms.push_back(&lock);
+                omp_init_lock(ms[i]);
+            }
             rs.push_back(std::make_shared<int64_t>(0));
         }
         std::chrono::time_point<chrono::system_clock> start, end;
 
         start = std::chrono::system_clock::now();
-        task_group tg;
-//        for(int i=0; i < concurrency; i++)
+//        for(int i=0; i < ff::rt::rt_concurrency(); i++)
         for(int i=0; i < concurrency * 60; i++)
         {
+            #pragma omp parallel for
             for(int j = 0; j < concurrency; j++)
             {
-
-                /*int t = rand()% concurrency;
-                tg.run([t,bIsStd]() {
-                    if(bIsStd)
-                        task_fun_std(t);
-                    else
-                        task_fun(t);
-                });*/
-                tg.run([j,bIsStd]() {
-                    if(bIsStd)
-                        task_fun_std(j);
-                    else
-                        task_fun(j);
-                });
+                if(bIsStd) {
+//					int t = rand()%ff::rt::rt_concurrency();
+//					ptf([t,bIsStd](){task_fun_std(t);});
+                    task_fun_std(j);
+                }
+                else {
+//					cout << "start task_fun i=" << i << " j=" << j << endl;
+//					int t = rand()%ff::rt::rt_concurrency();
+//					ptf([t](){task_fun(t);}, ms[t]->id());
+                    task_fun(j);
+//					cout << "end task_fun i=" << i << " j=" << j << endl;
+                }
             }
         }
-        tg.wait();
 
         end = std::chrono::system_clock::now();
         elapsed_seconds = std::chrono::duration_cast<chrono::microseconds>
                           (end-start).count();
-        cout << "tbb elapsed time: " << elapsed_seconds << "us" << endl;
+        cout << "ff elapsed time: " << elapsed_seconds << "us" << endl;
     }
     else {
         for(int i = 0; i< concurrency; i++)
@@ -167,6 +167,8 @@ int main(int argc, char *argv[])
         {
             for(int j = 0; j < concurrency; j++)
             {
+//				int t = rand()%concurrency;
+//               task_fun_serial(t);
                 task_fun_serial(j);
             }
         }
@@ -184,4 +186,3 @@ int main(int argc, char *argv[])
 //     }
     return 0;
 }
-
