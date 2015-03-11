cdba04a4646bd282d1503acb00ee891061141f2b
Xuepeng Fan <athrunarthur@gmail.com>
2015-03-11 17:48:54 +0800

    build

--- benchmark/run.sh
+++ benchmark/run.sh
@@ -2,17 +2,14 @@
 
 
 cd build && cmake ../ && make
+cp ../Makefile ./
 
-for i in $(seq 30)
+
+for i in $(seq 2)
 do
   echo $i
-  cd canny
-  ./canny_ff && cat time.json >> ../../time.json
-  cd ../
-
-  cd ../ && python build.py run res.json && cd command/ && mv ../res.json ./
-  python bomb.py res.json data.json  original
-  rm ../res.json 
+  make ff
 done
+
 
 cd ../
